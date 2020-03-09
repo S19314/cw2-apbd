c2214bd3d689f17554af6f89ae2f465b301f870e
s19314 <s19314@LAB223-12>
2020-03-09 17:00:43 +0100

    Pierwszy commit

---
+++ Cw2/Program.cs
@@ -0,0 +1,52 @@
+﻿using Cw2.Student_1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Cw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = //"Z:\\APBD\\GITHUB\\cw1-apbd3\\Cw2\\Cw2\\Data\\dane.csv";//
+            @"Data\dane.csv";
+
+            var fi = new FileInfo(path);
+            Console.WriteLine("SSSS " + fi.Directory);
+            //using ( 
+            var stream = new StreamReader(fi.OpenRead()); //))
+            { // using lubaja klasa, kotoraja imeet Dispose
+                // Dispose czasie wsego u waznych klas
+                string line = null;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+             stream.Dispose();
+
+            // XML  
+            //var list = new List<Student_1.Student>();
+            var list = new List<Student>();
+
+            var st = new Student
+            {
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email =  "kowalksi@wp.pl"
+            };
+            list.Add(st);
+
+            st.Email = "aa";
+            // typeof - poluczenia tipa, bez sozdania objekta
+
+            // pgago\studenci -> sxxxx/
+            FileStream writer = new FileStream(@"data.xml", FileMode.Create)
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Student>),
+                                                            new XmlRootAttribute("uczenlnia"));
+            xmlSerializer.Serialize(writer, list);
+        }
+    }
+}
---
+++ Cw2/Student_1/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cw2.Student_1
+{
+    public class Student
+    {   // prop + tabx2
+        // [XmlAttribute ] // atribut fila, a nie objeckta
+        public string Email { get; set; }
+
+        public string Imie { get; set; }
+
+        private string _nazwisko;
+        public string Nazwisko {
+            get { return _nazwisko; }
+            set{
+                if (value == null) throw new ArgumentException();
+                _nazwisko = value;
+            }
+           
+    }
+
+    }
+}
