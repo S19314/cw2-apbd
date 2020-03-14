0e74dc45f49d75dfc7e04ca27bd385c143a4b2d9
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2020-03-14 18:12:39 +0100

    Version 1.3

--- Cw2/Program.cs
+++ Cw2/Program.cs
@@ -31,123 +31,142 @@ namespace Cw2
             // formatDanych = ((args[2] == null) ||  (args[2] == "")) ? defaultTypDanych : args[2]; 
 
             sourceFilePath = @"D:\Projects\C#\APBD\Cwieczenie_2\Cw2\Data\dane.csv";
-            
+            aimFilePath = "C:/Users/admin/Desktop/Cw2_CS/result.xml";
+
 
             // formatDanych = (args[2] == "json") ? args[2] :"xml";
 
             // _--_ var workSheetTemplate = FactoryWriter.createWriter(args[2]);
-            
+
             //            ICollection<Student> 
-             var students = new List<Student>();
+            var students = new List<Student>();
             //               ICollection 
             //IList students =
 
             Dictionary<string, int> hashCourses = new Dictionary<string, int>();
             //using (FileStream logFileStream = new FileStream("log.txt", FileMode.Create) ){
-            using (StreamWriter logStreamWriter = new StreamWriter("log.txt", false, System.Text.Encoding.Default)) { 
-            using (var stream = new StreamReader(new FileInfo(sourceFilePath).OpenRead())) {
-                string line = null;
-                while ((line = stream.ReadLine()) != null)
+            using (StreamWriter logStreamWriter = new StreamWriter("log.txt", false, System.Text.Encoding.Default)) {
+                try
                 {
-                    string[] studentParametrs = line.Split(',');
 
-                        bool isMessageForLog = false;
-                string infoStudent = "";
-                for (int i = 0; i < studentParametrs.Length; i++)
-                    infoStudent += " " + studentParametrs[i];
 
-                        
-                        if (studentParametrs.Length < 9)
+                    using (var stream = new StreamReader(new FileInfo(sourceFilePath).OpenRead()))
                     {
+                        string line = null;
+                        while ((line = stream.ReadLine()) != null)
+                        {
+                            string[] studentParametrs = line.Split(',');
 
-                        infoStudent = "Student: " + infoStudent + " ma " + studentParametrs.Length + " parametrów, " +
-                            "a potrzebno mieć 9";
-                            logStreamWriter.Write(infoStudent + "\n");
-                            logStreamWriter.Flush();
-                            continue;
+                            bool isMessageForLog = false;
+                            string infoStudent = "";
+                            for (int i = 0; i < studentParametrs.Length; i++)
+                                infoStudent += " " + studentParametrs[i];
 
-                    }
-                    for(int  i = 0; i < studentParametrs.Length; i++)
-                    {
-                            if (studentParametrs[i] == null || studentParametrs[i] == "")
+                            if (studentParametrs.Length < 9)
                             {
-                                infoStudent += "; Ma pusty parametry";
-                                isMessageForLog = true;
-                                break;
+
+                                infoStudent = "Student: " + infoStudent + " ma " + studentParametrs.Length + " parametrów, " +
+                                    "a potrzebno mieć 9";
+                                logStreamWriter.Write(infoStudent + "\n");
+                                logStreamWriter.Flush();
+                                continue;
+
                             }
-                    }
-
-                    
-                        // Console.WriteLine("ska " + studentParametrs[4] + ", fname " + studentParametrs[0]);
-                    var studies = new Studies {
-                        Name = studentParametrs[2],
-                        Mode = studentParametrs[3]
-                    };
-
-                    var st = new Student
-                    {
-                        Ska = studentParametrs[4],
-                        Fname = studentParametrs[0],
-                        Lname = studentParametrs[1],
-                        Birthdate = studentParametrs[5],
-                        Email = studentParametrs[6],
-                        MothersName = studentParametrs[7],
-                        FathersName = studentParametrs[8],
-                        Studies = studies
-                    };
-
-                        /*
-                        st.Ska(studentParametrs[4]);
-                        st.setFname(studentParametrs[0]);
-                        st.setLname(studentParametrs[1]);
-                        st.setBirthdate(studentParametrs[5]);
-                        st.setEmail(studentParametrs[6]);
-                        st.setMothersName(studentParametrs[7]);
-                        st.setFathersName(studentParametrs[8]);
-                        st.SetStudies( new Studies(
-                                                   studentParametrs[2], 
-                                                   studentParametrs[3]
-                                                   )
-                        );
-                        */
-                        //,
-                        //                        studentParametrs[7],
-                        //                        studentParametrs[8]
-
-
-                        // ещё проверить на правильность пути
-
-
-                    for(int i = 0; i < students.Count; i++)
-                    {
-                            if (students[i].Equals(st)) {
-                                infoStudent += " ; Takij student już istnieje w Baze Dannych";
-                                isMessageForLog = true; 
+                            for (int i = 0; i < studentParametrs.Length; i++)
+                            {
+                                if (studentParametrs[i] == null || studentParametrs[i] == "")
+                                {
+                                    infoStudent += "; Ma pusty parametry";
+                                    isMessageForLog = true;
+                                    break;
+                                }
                             }
-                    }
 
-                    if (isMessageForLog)
-                    {
-                        logStreamWriter.Write(infoStudent + "\n");
-                        logStreamWriter.Flush();
-                        continue;
-                    }
 
-                    students.Add(st);
-                    string courseName = studentParametrs[2];// students[students.Count-1].Studies.Name;
-                    if (hashCourses.ContainsKey(courseName))
-                    {
-                        hashCourses[courseName]++;
-                    }
-                    else
-                    {
-                        hashCourses.Add(courseName, 1);
+                            // Console.WriteLine("ska " + studentParametrs[4] + ", fname " + studentParametrs[0]);
+                            var studies = new Studies
+                            {
+                                Name = studentParametrs[2],
+                                Mode = studentParametrs[3]
+                            };
+
+                            var st = new Student
+                            {
+                                Ska = studentParametrs[4],
+                                Fname = studentParametrs[0],
+                                Lname = studentParametrs[1],
+                                Birthdate = studentParametrs[5],
+                                Email = studentParametrs[6],
+                                MothersName = studentParametrs[7],
+                                FathersName = studentParametrs[8],
+                                Studies = studies
+                            };
+
+                            /*
+                            st.Ska(studentParametrs[4]);
+                            st.setFname(studentParametrs[0]);
+                            st.setLname(studentParametrs[1]);
+                            st.setBirthdate(studentParametrs[5]);
+                            st.setEmail(studentParametrs[6]);
+                            st.setMothersName(studentParametrs[7]);
+                            st.setFathersName(studentParametrs[8]);
+                            st.SetStudies( new Studies(
+                                                       studentParametrs[2], 
+                                                       studentParametrs[3]
+                                                       )
+                            );
+                            */
+                            //,
+                            //                        studentParametrs[7],
+                            //                        studentParametrs[8]
+
+
+                            // ещё проверить на правильность пути
+
+
+                            for (int i = 0; i < students.Count; i++)
+                            {
+                                if (students[i].Equals(st))
+                                {
+                                    infoStudent += " ; Takij student już istnieje w Baze Dannych";
+                                    isMessageForLog = true;
+                                }
+                            }
+
+                            if (isMessageForLog)
+                            {
+                                logStreamWriter.Write(infoStudent + "\n");
+                                logStreamWriter.Flush();
+                                continue;
+                            }
+
+                            students.Add(st);
+                            string courseName = studentParametrs[2];// students[students.Count-1].Studies.Name;
+                            if (hashCourses.ContainsKey(courseName))
+                            {
+                                hashCourses[courseName]++;
+                            }
+                            else
+                            {
+                                hashCourses.Add(courseName, 1);
+                            }
+                        }
+
                     }
                 }
+                catch (FileNotFoundException fnfe)
+                {
+                    logStreamWriter.WriteLine("Plik " + fnfe.FileName + " nie istnieje");
+                    logStreamWriter.Flush();
+                }
+                catch (DirectoryNotFoundException dnfe) {
+                    logStreamWriter.WriteLine("Podana ścieżka jest niepoprawna");
+                    logStreamWriter.Flush();
+                }
+                
             }
 
-            }
-            aimFilePath = "C:/Users/admin/Desktop/Cw2_CS/result.xml";
+            
             // Закинуть в реализацию интерфейса IWrite
 
 
@@ -218,48 +237,3 @@ namespace Cw2
     }
 
 }
-
-
-
-
-/*
-    string path = //"Z:\\APBD\\GITHUB\\cw1-apbd3\\Cw2\\Cw2\\Data\\dane.csv";//
-            @"Data\dane.csv";
-
-            var fi = new FileInfo(path);
-            Console.WriteLine("SSSS " + fi.Directory);
-            //using ( 
-            var stream = new StreamReader(fi.OpenRead()); //))
-            { // using lubaja klasa, kotoraja imeet Dispose
-                // Dispose czasie wsego u waznych klas
-                string line = null;
-                while ((line = stream.ReadLine()) != null)
-                {
-                    Console.WriteLine(line);
-                }
-            }
-             stream.Dispose();
-
-            // XML  
-            //var list = new List<Student_1.Student>();
-            var list = new List<Student>();
-
-            var st = new Student
-            {
-                Imie = "Jan",
-                Nazwisko = "Kowalski",
-                Email =  "kowalksi@wp.pl"
-            };
-            list.Add(st);
-
-            st.Email = "aa";
-            // typeof - poluczenia tipa, bez sozdania objekta
-
-            // pgago\studenci -> sxxxx/
-            FileStream writer = new FileStream(@"data.xml", FileMode.Create)
-            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Student>),
-                                                            new XmlRootAttribute("uczenlnia"));
-            xmlSerializer.Serialize(writer, list);
-        
-
-     */
